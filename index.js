a508785b88a716e1d38d3e02985642bbb2f6ec9a
William John C Rivera <105565558+WIMWHAG@users.noreply.github.com>
2022-07-07 21:24:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const { nextISSTimesForMyLocation } = require('./iss');
    if (error) {
      return console.log("It didn't work!", error);
    }
-   // success, print out the deets!
    console.log(passTimes);
  });
